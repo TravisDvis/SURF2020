f0e30534a04f8010c45cf76ea33d27447fd6687a
Travis Davis <t3davis@ucsd.edu>
2020-07-14 18:13:30 -0700

    RK4 and Reversal Detection

--- myRK4.py
+++ myRK4.py
@@ -9,13 +9,37 @@ def myRK4(x0,dt,nSteps):
     x = x0
     xMatrix = x0
 
+    t = 0
+    reversal_times = np.array([])
+
     for kk in range(nSteps):
+        
+        t = t + dt
+
         k1 = f(x)
         k2 = f(x + 0.5*dt*k1)
         k3 = f(x + 0.5*dt*k2)
         k4 = f(x + dt*k3)
         
+        if x[1] >= 0:
+            sign = 0
+        else:
+            sign = 1
+
         x = x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+        if sign == 0 and x[1] < 0:
+            print("A dipole reversal has occurred")
+            print(t)
+            reversal_times = np.append(reversal_times,t)
+            print(reversal_times)            
+
+        elif sign == 1 and x[1] >= 0:
+            print("A dipole reversal has occurred")
+            print(t)
+            reversal_times = np.append(reversal_times,t)
+            print(reversal_times)
+
         xMatrix = np.append(xMatrix,x,axis = 1)
 
     return xMatrix
