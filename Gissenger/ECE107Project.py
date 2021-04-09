6542fba7179398347b151e2941e10c6ba51fb7bc
Travis Davis <t3davis@ucsd.edu>
2021-04-09 13:12:36 -0700

    Varied window size to see how size affected the optimal MCC

---
+++ Gissenger/ECE107Project.py
@@ -0,0 +1,10 @@
+from scipy.constants import *
+from FloatExceptionHandle import *
+
+v0 = floatExceptionHandle("Enter a value for v0: ",1)
+
+vtop = -v0/2
+vbot = v0/2
+
+elec_cons = 1/(4*pi*epsilon_0)
+
